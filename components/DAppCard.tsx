926b86bae838bb544021c5fa0e8e852507561bb8
otctoken <pppwjjj@gmail.com>
2026-01-20 16:52:54 +0800

    26.1.20.1752

--- components/DAppCard.tsx
+++ components/DAppCard.tsx
@@ -152,7 +152,7 @@ const DAppCard: React.FC<DAppCardProps> = ({ dapp }) => {
                             <h3 className="font-bold text-xl text-slate-100">{dapp.name}</h3>
                             <span className="text-xs text-slate-400 bg-slate-800 px-2 py-0.5 rounded-md border border-slate-700">{dapp.category}</span>
                         </div>
-                        <p className="text-slate-400 text-xs mt-0.5 line-clamp-1">{dapp.contract}</p>
+                        <p className="text-slate-400 text-xs mt-0.5 break-all text-balance">Contract:{dapp.contract}</p>
                     </div>
                 </div>
 
